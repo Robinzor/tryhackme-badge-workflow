30c8f3eb5431b1d45112ddb25d177228d4ce4dd8
Robin Visser <robinvisser92@live.nl>
2024-03-21 21:00:36 +0100

    Update capture-screenshot.js

--- capture-screenshot.js
+++ capture-screenshot.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const browser = await puppeteer.launch({ executablePath: '/usr/bin/google-chrome', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
 
   await page.setContent(`<html><body><script src="https://tryhackme.com/badge/1604750"></script></body></html>`, { waitUntil: 'networkidle0' });
